c7839bdd27c365ef2bdd9ead588b19840954c70e
Anton <be1337@live.ru>
2021-10-08 19:55:08 +0300

    Try merge

---
+++ src/Technology.js
@@ -0,0 +1,13 @@
+const Technology = () => {
+    return (
+        <div>
+            <ul>
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>CSS</li>
+                <li>HTML</li>
+            </ul>
+        </div>);
+}
+
+export default Technology;
